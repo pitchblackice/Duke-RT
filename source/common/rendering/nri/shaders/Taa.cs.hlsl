813cf56168183391c08ee170c89da3ab5315fd3c
brian <postmemetic@gmail.com>
2026-03-20 11:54:33 -0700

    Bypass NRI temporal accumulation for bring-up

--- source/common/rendering/nri/shaders/Taa.cs.hlsl
+++ source/common/rendering/nri/shaders/Taa.cs.hlsl
@@ -13,17 +13,5 @@ void main(uint3 dispatchThreadId : SV_DispatchThreadID)
 	const float2 uv = ((float2)pixelPos + 0.5) / resolution;
 
 	const float4 currentColor = gComposedInput[pixelPos];
-	const float2 motion = gMotionInput[pixelPos].xy;
-	const float2 previousUv = uv + motion / resolution;
-
-	float4 historyColor = currentColor;
-	float historyWeight = 0.0;
-	if ((gTraceConstants.Flags & 0x1u) == 0 && all(previousUv >= 0.0) && all(previousUv <= 1.0))
-	{
-		historyColor = gHistoryInput.SampleLevel(gLinearClamp, previousUv, 0.0);
-		const float motionMagnitude = length(motion);
-		historyWeight = saturate(0.92 - motionMagnitude * 0.04);
-	}
-
-	gHistoryOutput[pixelPos] = lerp(currentColor, historyColor, historyWeight);
+	gHistoryOutput[pixelPos] = currentColor;
 }
