6649ec5cf47e1d4f62f901f85debef16d82e3923
brian <postmemetic@gmail.com>
2026-03-19 17:01:04 -0700

    Fix NRI 2D position transform

--- source/common/rendering/nri/shaders/Nri2D.vs.hlsl
+++ source/common/rendering/nri/shaders/Nri2D.vs.hlsl
@@ -16,7 +16,8 @@ VSOutput main(VSInput input)
 {
 	VSOutput output;
 
-	float4 p = NriTransformPosition(float3(input.Position.x, input.Position.z, input.Position.y));
+	// F2DDrawer uploads screen-space vertices as (x, y, z). Preserve that order here.
+	float4 p = NriTransformPosition(input.Position);
 	float2 ndc = float2(p.x * gNri2DConstants.InvViewportSize.x * 2.0 - 1.0, 1.0 - p.y * gNri2DConstants.InvViewportSize.y * 2.0);
 
 	output.Position = float4(ndc, p.z, 1.0);
