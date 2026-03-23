cd538c24de7054aa04ee3f256a9c91f6d2ea48c6
brian <postmemetic@gmail.com>
2026-03-22 22:51:55 -0700

    Validate PT sky texture pointers with VirtualQuery

--- source/common/rendering/nri/scene/nri_portal_bridge.cpp
+++ source/common/rendering/nri/scene/nri_portal_bridge.cpp
@@ -26,9 +26,40 @@ namespace
 	bool IsUsableGameTexturePointer(FGameTexture* texture)
 	{
 		const uintptr_t value = (uintptr_t)texture;
-		return value > 0x10000 &&
-			value != (uintptr_t)-1 &&
-			(value & (sizeof(void*) - 1)) == 0;
+		if (value <= 0x10000 ||
+			value == (uintptr_t)-1 ||
+			(value & (sizeof(void*) - 1)) != 0)
+		{
+			return false;
+		}
+
+		MEMORY_BASIC_INFORMATION pointerInfo = {};
+		if (VirtualQuery(texture, &pointerInfo, sizeof(pointerInfo)) != sizeof(pointerInfo) ||
+			pointerInfo.State != MEM_COMMIT ||
+			(pointerInfo.Protect & (PAGE_NOACCESS | PAGE_GUARD)) != 0)
+		{
+			return false;
+		}
+
+		void* vtable = nullptr;
+		__try
+		{
+			vtable = *(void**)texture;
+		}
+		__except (EXCEPTION_EXECUTE_HANDLER)
+		{
+			vtable = nullptr;
+		}
+
+		if (vtable == nullptr)
+		{
+			return false;
+		}
+
+		MEMORY_BASIC_INFORMATION vtableInfo = {};
+		return VirtualQuery(vtable, &vtableInfo, sizeof(vtableInfo)) == sizeof(vtableInfo) &&
+			vtableInfo.State == MEM_COMMIT &&
+			(vtableInfo.Protect & (PAGE_NOACCESS | PAGE_GUARD)) == 0;
 	}
 
 	void TranslateSurface(SurfaceRef& surface, const float delta[3])
