e81be586b33f19eb9dae751b60c07b7e8893808c
brian <postmemetic@gmail.com>
2026-05-13 19:32:27 -0700

    Force RT backend in Duke launcher

--- package/windows/launch-duke-rt.cmd
+++ package/windows/launch-duke-rt.cmd
@@ -34,6 +34,7 @@ if not exist "%PREFLIGHT_PS1%" (
 
 set "PREFLIGHT_ARGS="
 set "GAME_ARGS="
+set "RT_MODE_ARGS=+set vid_preferbackend 4"
 
 :parse_args
 if "%~1"=="" goto args_done
@@ -154,5 +155,5 @@ if not exist "%DUKE_RT_GRP%" (
     exit /b 1
 )
 
-"%RAZE_EXE%" -gamegrp "%DUKE_RT_GRP%" -file "%OVERLAY_DIR%" %GAME_ARGS%
+"%RAZE_EXE%" -gamegrp "%DUKE_RT_GRP%" -file "%OVERLAY_DIR%" %GAME_ARGS% %RT_MODE_ARGS%
 exit /b %errorlevel%
