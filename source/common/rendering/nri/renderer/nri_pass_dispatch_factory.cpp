5f03e9af5b0305453c06d5b8a2620d3472e862db
brian <postmemetic@gmail.com>
2026-07-04 10:57:24 -0700

    Use bound scene records for trace instance count

--- source/common/rendering/nri/renderer/nri_pass_dispatch_factory.cpp
+++ source/common/rendering/nri/renderer/nri_pass_dispatch_factory.cpp
@@ -281,7 +281,7 @@ NRIPassDispatchContext NRIRenderer::BuildPassDispatchContext()
 	std::copy(mGroundColor, mGroundColor + 3, init.frame.groundColor.begin());
 	init.frame.guiCaptureActive = mGuiCaptureActive;
 	init.frame.resetHistory = mResetHistory;
-	init.sceneStats.sceneInstanceCount = mSceneInstanceBuffer.stride != 0 ? (uint32_t)(mSceneInstanceBuffer.usedSize / mSceneInstanceBuffer.stride) : 0u;
+	init.sceneStats.sceneInstanceCount = (uint32_t)mBoundSceneInstances.size();
 	init.sceneStats.staticPrimitiveCount = mBoundStaticPrimitiveCount;
 	init.sceneStats.dynamicPrimitiveCount = mBoundDynamicPrimitiveCount;
 	init.sceneStats.staticMaterialCount = mBoundStaticMaterialCount;
